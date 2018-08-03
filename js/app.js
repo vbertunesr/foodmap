2399be68b9a49142e56b84e385675ce695b2fd72
Valeria Bertunes <bertunesrocha@gmail.com>
2018-08-03 14:55:45 -0300

    mapa ok

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-// Add your JavaScript
+$(document).ready(function(){
+    $(".splash").delay(2500).fadeOut('slow');
+    $(".initial-page").hide();
+    $(".initial-page").delay(2500).fadeIn('slow');
+
+var mapMobile;
+
+function initMap() {
+    mapMobile = new google.maps.Map(document.getElementById('map-mobile'), {
+    center: {lat: -11.606968, lng: -77.059364},
+    zoom: 10
+    });
+}
+
+initMap();
+
+});
+
